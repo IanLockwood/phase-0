19889db5404f1525e17e29b9542a5763416f11f7
Ian Lockwood <mrianlockwood@gmail.com>
2015-09-13 18:08:44 -0400

    Add 4.7

---
+++ week-4/factorial/my_solution.rb
@@ -0,0 +1,19 @@
+# Factorial
+
+# I worked on this challenge with: Ricky Hu.
+
+
+# Take in the number
+# result = 1
+# While number is > 0, multiply number by result and then set number to (number - 1).
+# When number reaches 0, return result.
+
+# Your Solution Below
+def factorial(number)
+	result = 1
+	while number > 0
+		result = number * result
+		number = number - 1
+	end
+	return result
+end
