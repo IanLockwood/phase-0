240deff0d5d5bb2117798f68a6f4368d6ffa9f1e
Ian Lockwood <mrianlockwood@gmail.com>
2015-09-15 17:34:39 -0400

    Rename solo challenge

--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -12,10 +12,21 @@
 # What is the output? (i.e. What should the code return?)
 # What are the steps needed to solve the problem?
 
+# Take in integer
+# IF it is < 1000
+# RETURN the integer
+# ELSE Convert the integer to a string with to_s
+# Use the INSERT method and iterate to insert commas in the -4, -8, -12, etc. position.
+# return the string
+
 
 # 1. Initial Solution
 
-
+def separate_comma(num)
+	if num < 1000
+		return num.to_s
+	end
+end
 
 
 # 2. Refactored Solution
