f0b9e46a6abcf074cfdaeb5f9f46afba47f53336
Ian Lockwood <mrianlockwood@gmail.com>
2015-09-15 13:08:06 -0400

    Add assignments for weeks 5-9

---
+++ week-7/group-project/my_solution.js
@@ -0,0 +1,5 @@
+ // JavaScript Telephone
+
+// My role in the group is:
+
+// Here is my part of the challenge:
---
+++ week-7/nums_commas.js
@@ -0,0 +1,25 @@
+// Separate Numbers with Commas in JavaScript **Pairing Challenge**
+
+
+// I worked on this challenge with: .
+
+// 0. Pseudocode
+
+
+
+// 1. Initial Solution
+
+
+
+
+// 3. Refactored Solution
+
+
+
+
+// 2. Your Own Tests (OPTIONAL)
+
+
+
+
+// 4. Reflection
