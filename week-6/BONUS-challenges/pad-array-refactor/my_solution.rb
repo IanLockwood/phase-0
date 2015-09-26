b25fbb25ef7d1247b4badeae3dc66fc4eb8378c7
Ian Lockwood <mrianlockwood@gmail.com>
2015-09-26 19:57:00 -0400

    Finish reflection

--- week-6/BONUS-challenges/pad-array-refactor/my_solution.rb
+++ week-6/BONUS-challenges/pad-array-refactor/my_solution.rb
@@ -29,3 +29,59 @@
 
 
 # 5. Reflection
+
+
+
+
+class CreditCard
+
+  def initialize(number)
+    @num = number
+    @total = 0
+    if number.to_s.length != 16
+      raise ArgumentError.new("Your number must be 16 digits")
+    end
+  end
+
+  def double
+    array_credit = []
+    array_credit = @num.to_s.split('')
+    array_credit.map! {|x| x.to_i}
+    @num = array_credit.each_with_index.map do |x,y|
+
+      if y % 2 == 0
+        x * 2
+      else
+        x
+      end
+    end
+  end
+
+  def split_digits
+    digits = []
+    @num = @num.join
+    digits = @num.to_s.split('')
+    @num = digits.map! {|x| x.to_i}
+  end
+
+
+
+  def sum
+    @num.each {|x| @total = x + @total}
+    return @total
+  end
+
+  def check_card
+  @total % 10 == 0
+  end
+  
+end
+
+
+
+
+
+card = CreditCard.new(4408041234567906)
+
+
+p card.initialize(4408041234567906)
--- week-6/gps.rb
+++ week-6/gps.rb
@@ -1,35 +1,60 @@
 # Your Names
-# 1)
-# 2)
+# 1) Ian Lockwood
+# 2) Ryan Lesson
 
-# We spent [#] hours on this challenge.
+# We spent [1] hours on this challenge.
 
 # Bakery Serving Size portion calculator.
 
 def serving_size_calc(item_to_make, order_quantity)
   library = {"cookie" => 1, "cake" =>  5, "pie" => 7}
-  error_counter = 3
+  
+  # remove error counter
+  # compared library key "item_to_make" to nil
+  # if == nil raise ArgumentError
 
-  library.each do |food|
-    if library[food] != library[item_to_make]
-      p error_counter += -1
-    end
+
+    if library[item_to_make] == nil
+      raise ArgumentError.new("#{item_to_make} is not a valid input")
   end
 
-  if error_counter > 0
-    raise ArgumentError.new("#{item_to_make} is not a valid input")
-  end
-
-  serving_size = library.values_at(item_to_make)[0]
+  #hashed the keys value of library for the value of the variable serving size
+  #Find modulus of Order_quantity/serving size
+  serving_size = library[item_to_make]
   serving_size_mod = order_quantity % serving_size
 
-  case serving_size_mod
-  when 0
+  
+  
+  #Remove case statement and replaced it with a simpler if/elsif/else statement
+  #Added suggestion function
+  #Pushed suggestion keys into an array
+  suggested=[]
+  if serving_size_mod > 0
+    library.each do |key,value|
+      if serving_size_mod >= value
+        
+        suggested << key
+      end
+    end  
+      return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you have #{serving_size_mod} leftover ingredients. Suggested baking items:#{suggested}"
+  
+  elsif serving_size_mod ==0
     return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}"
+  
   else
-    return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you have #{serving_size_mod} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
+    "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you do not have enough ingredients"
   end
+
 end
+    
+  
+#   case serving_size_mod
+#   when 0
+#     return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}"
+#   else
+#     return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you have #{serving_size_mod} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
+#   end
+# end
 
 p serving_size_calc("pie", 7)
 p serving_size_calc("pie", 8)
@@ -40,5 +65,20 @@ p serving_size_calc("cookie", 10)
 p serving_size_calc("THIS IS AN ERROR", 5)
 
 #  Reflection
+
+# What did you learn about making code readable by working on this challenge?
+#   It's tough! Elaborating too much can make everything look messy, so you have to be concise and format well.
+#   Also, unreadable code is a huge problem when you're working with legacy code.
+
+# Did you learn any new methods? What did you learn about them?
+#   No...unless...was this the first time I used ArgumentError? 
+#   Well ArgumentError is a neat way to guide user input, and I've been told it gets more interesting with error handling.
+
+# What did you learn about accessing data in hashes? 
+#   Iteration makes it easier, and so does setting good variables!
+
+# What concepts were solidified when working through this challenge?
+#   Hashes, complex and nested methods, user interaction, and legacy code.
+
 
 
